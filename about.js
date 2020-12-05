eb9d08fac30514719ca87598ec8dcd80a215ac6a
Nauval <nauval@kredibel.com>
2020-12-05 17:32:30 +0700

    done

---
+++ about.js
@@ -0,0 +1,77 @@
+module.exports = {
+	header: 'NAUVAL',
+	tagline: `I able to think, design, code, \ndeploy, and sleep. Love to \n learn something new.\n\n⬇`,
+	sections: [
+		{
+			title: '👨 About Me',
+			body: `
+				Hi, I'm Muhamad Nauval Azhar.
+
+				I was born in the mid-97s. Programming since high school. 
+				Worked as a programmer since high school. Already completed 
+				many projects. Sublime Text, Stack Overflow, and DevTools 
+				are my beloved friends. I love using the Service Pattern design 
+				and BEM methodology. Open source was pleasant; my most favorite 
+				projects are Stisla and Kodinger. 
+
+				I 💙 blogging, teaching, and cycling.
+			`
+		},
+		{
+			title: '💻 Tech Stack',
+			body: `
+				Here's my current tech stack:\n
+				▶  JavaScript   => Node, React, Next, TypeScript, Puppeteer\n
+				▶  PHP          => Laravel, CodeIgniter, Lumen\n
+				▶  CSS          => Sass, PostCSS, Tailwind, Bootstrap, Bulma\n
+				▶  Design       => Figma, AI\n
+				▶  Web Server   => Nginx, Apache\n
+				▶  DBMS         => MySQL, Mongo \n
+				▶  Deployment   => Digital Ocean, Vercel, Netlify, cPanel\n
+				▶  Editor       => Sublime, VS Code, Vim\n
+				▶  OS           => Ubuntu Dekstop/Server, Windows, macOS\n
+				▶  Browser      => Firefox, Chrome\n
+				The above technologies that I am confident when using it. I've 
+				also used several other technologies for experimental and side projects, 
+				such as Python, Go, Svelte, Vue, React Native, Ionic and others. 
+			`
+		},
+		{
+			title: '📐 Projects',
+			body: `
+				Sometimes I create small open source projects, such as a file manager 
+				for Laravel, Bootstrap 4 based login page, and many more. 
+				But, here are some projects I'm proud of:\n
+				💡 Kodinger    => Indonesian Dev Community (https://kodinger.com)\n
+				📏 Stisla      => Free Admin Template (https://getstisla.com)\n
+				📹 Multinity   => My affordable courses (https://multinity.com)\n
+				📘 TailwindCSS => A book about Tailwind CSS (https://tailwindcss.id)\n
+				You can find my other projects on my GitHub 
+				or at https://nauv.al/projects.
+			`
+		},
+		{
+			title: '🌐 Internet',
+			body: `
+				To access this interface, you will definitely use the Internet. 
+				Therefore you can also find me on the internet through 
+				the following platforms:\n
+				🐦 Twitter => https://twitter.com/mhdnauvalazhar\n
+				👬 Facebook => https://facebook.com/mhdnauvalazhar\n
+				🐱 GitHub => https://github.com/nauvalazhar\n
+				🏀 Dribbble => https://dribbble.com/mhdnauvalazhar\n
+				📼 Youtube => https://youtube.com/c/nauvalazhar\n
+			`
+		},
+		{
+			title: '👋 Contact Me',
+			body: `
+				If you have a cool project, hire me, or just questions, 
+				feel free to contact me at:\n
+				📬 nauvalazhar2@gmail.com\n
+				🌐 https://nauv.al (/profile to download my CV)\n
+				Thank you for reaching out to me.
+			`
+		}
+	],
+}
---
+++ index.js
@@ -0,0 +1,64 @@
+const figlet = require('figlet');
+const center = require('center-align');
+const fs = require('fs');
+const path = require('path');
+const about = require('./about.js');
+
+// pl/pr=padding-left/right
+const [pl, pr, width, marks] = ["   ", "   ", 80, {
+	nw: "╔",
+	n:  "═",
+	ne: "╗",
+	e:  "║",
+	se: "╝",
+	s:  "═",
+	sw: "╚",
+	w:  "║",
+}];
+
+// helpers
+// repeat alias
+const r = (str, count) => str.repeat(count);
+// create custom box for about section and this box is autoheight
+const bx = (title, body) => {
+	let o = `${marks.nw}${marks.n}${title}${r(marks.n, (width) - title.length - 3)}${marks.ne}\n`;
+
+	const bs = body.trim().split("\n");
+
+	const bl = marks.w + r(" ", width - 2) + marks.w + "\n";
+
+	o += bl;
+	bs.forEach(l => {
+		const t = l.trim();
+		const restTotal = width - t.length - (pl + pr).length - 2;
+		const rest = restTotal > -1 ? r(" ", restTotal) : '';
+
+		o += marks.w + pl + t + rest + pr + marks.w+ "\n";
+	});
+	o += bl;
+
+	o += `${marks.sw}${r(marks.s, width - 2)}${marks.se}\n`
+
+	return o;
+}
+
+// use custom figlet font (named 3d.flf inside fonts/ directory)
+const data = fs.readFileSync(path.join(__dirname, '3d.flf'), 'utf8');
+figlet.parseFont('3dFont', data);
+
+// starting point
+figlet.text(about.header, {
+	font: '3dFont'
+}, function(err, headerText) {
+	console.log("\n\n");
+	console.log(
+		center(headerText, width),
+		"\n\n",
+		center(about.tagline, width),
+		"\n\n"
+	);
+
+	about.sections.forEach(({title, body}) => {
+		console.log(bx(title, body));
+	});
+})
